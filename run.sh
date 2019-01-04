5cb936f47cbb51a9537d17674b802934c33ad9b6
Tyler Baker <tyler@foundries.io>
2019-01-03 19:22:06 -0800

    Add monerod helper
    Signed-off-by: Tyler Baker <tyler@foundries.io>

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@ docker pull fullnodes/monero:latest
 docker stop monerod
 docker rm monerod
 docker volume create monero
-docker run -dit --name monerod --restart=always -v monero:/root/.bitmonero fullnodes/monero:latest
+docker run -dit --name monerod --net=host --restart=always -v monero:/root/.bitmonero fullnodes/monero:latest
+mkdir -p ~/.bin
+cp $(pwd)/bin/monero* ~/.bin
